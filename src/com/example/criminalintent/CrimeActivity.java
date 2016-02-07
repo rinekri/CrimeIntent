dee6b37ade618a6265b598b51a26c934ec15101f
ilya <MAAFI4I@gmail.com>
2016-02-07 19:47:37 +0200

    After starting the project

---
+++ src/com/example/criminalintent/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.example.criminalintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CrimeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
+	}
+}
