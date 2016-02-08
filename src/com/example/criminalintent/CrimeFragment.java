b56bcedfd40be9f92856080959c009117f81eb96
ilya <MAAFI4I@gmail.com>
2016-02-08 20:59:18 +0200

    After adding the addTextChangedListener for EditText in CrimeFragment

--- src/com/example/criminalintent/CrimeFragment.java
+++ src/com/example/criminalintent/CrimeFragment.java
@@ -2,13 +2,17 @@ package com.example.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 	
-	Crime mCrime;
+	private Crime mCrime;
+	private EditText mEditText;
 	
 	@Override
 	public void onCreate(Bundle savedFragmentState) {
@@ -20,6 +24,29 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater layoutInflater, ViewGroup parent, Bundle savedFragmentState) {
 		View view = layoutInflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		
+		mEditText = (EditText) view.findViewById(R.id.crime_title);
+		mEditText.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				mCrime.setTitle(s.toString());
+			}
+		});
+		
 		return view;
 	}
 	
