c76df2fa4c4b10976ca206ca8ac5a23fb8260d4f
ilya <MAAFI4I@gmail.com>
2016-02-08 20:26:31 +0200

    After overriding the method onCreateView() in CrimeFragment

--- src/com/example/criminalintent/CrimeFragment.java
+++ src/com/example/criminalintent/CrimeFragment.java
@@ -2,15 +2,25 @@ package com.example.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeFragment extends Fragment {
 	
 	Crime mCrime;
 	
+	@Override
 	public void onCreate(Bundle savedFragmentState) {
 		super.onCreate(savedFragmentState);
 		mCrime = new Crime();
 		
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater layoutInflater, ViewGroup parent, Bundle savedFragmentState) {
+		View view = layoutInflater.inflate(R.layout.fragment_crime, parent, false);
+		return view;
 	}
 	
 }
