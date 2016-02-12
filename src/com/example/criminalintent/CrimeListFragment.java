2aca97800cb8298e3b71b788d19b005b7497b9cb
ilya <MAAFI4I@gmail.com>
2016-02-12 19:32:18 +0200

    After adding the adapter for CrimeListFragment with standart behavior

--- src/com/example/criminalintent/CrimeListFragment.java
+++ src/com/example/criminalintent/CrimeListFragment.java
@@ -1,15 +1,21 @@
 package com.example.criminalintent;
 
 import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 public class CrimeListFragment extends ListFragment {
-	ArrayList<Crime> crimes;
+	ArrayList<Crime> mCrimes;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActivity().setTitle(R.string.crimes_title);
-        crimes = CrimeLab.get(getContext()).getCrimes();
+        mCrimes = CrimeLab.get(getActivity()).getCrimes();
+        
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),android.R.layout.simple_list_item_1, mCrimes);
+        setListAdapter(adapter);
+        
 	}
 }
