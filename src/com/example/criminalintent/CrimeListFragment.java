c53997f3403af234a4abc3e6f16066f4a5c8275c
ilya <MAAFI4I@gmail.com>
2016-02-13 19:48:19 +0200

    Created the inner class CrimeAdapter with our view and then used in the CrimeListFragment

--- src/com/example/criminalintent/CrimeListFragment.java
+++ src/com/example/criminalintent/CrimeListFragment.java
@@ -1,13 +1,18 @@
 package com.example.criminalintent;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class CrimeListFragment extends ListFragment {
 	private static final String TAG = "CrimeListFragment";
@@ -19,12 +24,42 @@ public class CrimeListFragment extends ListFragment {
 		getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
         
-        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),android.R.layout.simple_list_item_1, mCrimes);
+        CrimeAdapter adapter = new CrimeAdapter(mCrimes);
         setListAdapter(adapter);
 	}
 	
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		Crime c = (Crime) getListAdapter().getItem(position);
-		Log.d(TAG, c.getTitle() + " was clicked");
+		Crime c = ((CrimeAdapter) getListAdapter()).getItem(position);
+		Log.d(TAG, (c.getTitle() + " was clicked") + " with " +c.getDate() + " while " +c.isSolved());
+	}
+	
+	
+	public class CrimeAdapter extends ArrayAdapter<Crime> {
+
+		public CrimeAdapter(ArrayList<Crime> crimes) {
+			super(getActivity(), 0, crimes);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime, null);
+			}
+			
+			Crime c = getItem(position);
+			
+			TextView titleTextView = (TextView) convertView.findViewById(R.id.crime_list_item_titleTextView);
+			titleTextView.setText(c.getTitle());
+			
+			TextView dateTextView = (TextView) convertView.findViewById(R.id.crime_list_item_dateTextView);
+			String formatedDate = new SimpleDateFormat("d MMM yyyy, EEEE", Locale.getDefault()).format(c.getDate());
+			dateTextView.setText(formatedDate);
+			
+			CheckBox solvedCheckBox = (CheckBox) convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+			solvedCheckBox.setChecked(c.isSolved());
+			
+			return convertView;
+		}
 	}
 }
