061818f8536fad6f701b3dd6e6a0781356e1e894
ilya <MAAFI4I@gmail.com>
2016-02-14 21:16:40 +0200

    Added the new Activity as CrimePagerActivity

---
+++ src/com/example/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,16 @@
+package com.example.criminalintent;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class CrimePagerActivity extends FragmentActivity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+}
