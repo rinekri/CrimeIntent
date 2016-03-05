d4ff12b8dffa4a3e7adec452f9b11f8968ed2951
ilya <MAAFI4I@gmail.com>
2016-03-05 21:36:04 +0200

    Added a method to save list of Crimes

--- src/com/example/criminalintent/CrimeLab.java
+++ src/com/example/criminalintent/CrimeLab.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import android.content.Context;
+import android.util.Log;
 
 public class CrimeLab {
+	private static final String TAG = "CrimeLab";
+	private static final String FILENAME = "crimes.json";
+	
 	private static CrimeLab sCrimeLab;
 	private Context mAppContext;
 	
 	private ArrayList<Crime> mCrimes;
+	private CriminalIntentJSONSerializer mSerializer;
 	
 	private CrimeLab(Context appContext) {
 		mAppContext = appContext;
@@ -37,5 +42,16 @@ public class CrimeLab {
 	
 	public void addCrime(Crime c) {
 		mCrimes.add(c);
+	}
+	
+	public boolean saveCrimes() {
+		try {
+			mSerializer.saveCrimes(getCrimes());
+			Log.d(TAG, "Crimes saved to file");
+			return true;
+		} catch (Exception e) {
+			Log.e(TAG, "Error saving crimes: ", e);
+			return false;
+		}
 	}
 }
