f3aecdd204c31f3d3db53c698ff8c7c3a6994e71
ilya <MAAFI4I@gmail.com>
2016-03-08 20:38:21 +0200

    Added a mark for daughters of ListView

--- gen/com/example/criminalintent/R.java
+++ gen/com/example/criminalintent/R.java
@@ -1990,8 +1990,9 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020042;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020043;
         public static final int abc_textfield_search_material=0x7f020044;
-        public static final int ic_launcher=0x7f020045;
-        public static final int notification_template_icon_bg=0x7f020046;
+        public static final int background_activated=0x7f020045;
+        public static final int ic_launcher=0x7f020046;
+        public static final int notification_template_icon_bg=0x7f020047;
     }
     public static final class id {
         public static final int action0=0x7f090059;
